c9e8a51c7d35da0a83bf6d9758dcab302a2ce604
Thomas Andraos <104111958+domce69@users.noreply.github.com>
2023-07-12 19:15:45 +1000

    new

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,71 @@
+import { BsFillMoonStarsFill } from 'react-icons/bs';
+import { AiFillLinkedin, AiFillGithub, AiFillMail } from 'react-icons/ai'
+import Image from 'next/image';
+import devtom from "../public/tom.jpg";
+import design from '../public/design.png';
+import code from '../public/code.png';
+import consulting from '../public/consulting.png';
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
+    <div>
+      <main className='bg-white px-10 '>
+        <section className='min-h-screen'>
+          <nav className='py-10 mb-12 flex justify-between'>
+            <h1 className='text-xl font-medium'>Thomas Andraos</h1>
+            <ul className='flex items-center'>
+              <li><BsFillMoonStarsFill className='cursor-pointer text-2xl' /></li>
+              <li><a className='bg-gradient-to-r from-cyan-500 to-teal-500 text-white px-4 py-2 rounded-md ml-8' href="#">Resume</a></li>
+            </ul>
           </nav>
+          <div className='text-center p-10'>
+            <h2 className='text-5xl py-2 text-teal-600 font-medium'>Thomas Andraos</h2>
+            <h3 className='text-2xl py-2'>Developer and engineer.</h3>
+            <p className='text-md py-5 leading-8 text-gray-800'>
+              Bachelor of Computer Science graduate with a major in Software Engineering.
+            </p>
+          </div>
+          <div className='text-5xl flex justify-center gap-16 py-3 text-gray-600'>
+            <AiFillLinkedin />
+            <AiFillGithub />
+            <AiFillMail />
+          </div>
+          <div className=' relative w-80 h-80'>
+            <Image src={devtom} alt='' className='relative rounded-full' layout='fill' />
+          </div>
+        </section>
+
+        <section>
+          <div>
+            <h3 className='text-3xl py-1'>Learning and Experience</h3>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+              Education section here: talk about learning
+            </p>
+            <ul className='text-gray-800 py-1'>
+              <li>Java</li>
+              <li>HTML</li>
+              <li>jQuery</li>
+              <li>JavaScript</li>
+            </ul>
+            <p className='text-md py-2 leading-8 text-gray-800'>
+              training section here: talk abt training
+            </p>
+          </div>
+          <div>
+            <div className='text-center shadow-lg p-10 rounded-xl my-10'>
+                <Image src={design} alt='' width={100} height={100} />
+              <h3 className='text-lg font-medium pt-8 pb-2'>Innovative Designs</h3>
+              <p></p>
+              <h4 className='py-4 text-teal-600'>Design Tools I Use</h4>
+              <ul className='text-gray-800 py-1'>
+                <li>Photoshop</li>
+                <li>Illustrator</li>
+                <li>Figma</li>
+              </ul>
+            </div>
+          </div>
         </section>
       </main>
     </div>
-    </main>
-  )
+  );
 }
